3b4d371c7d7cc35df8e6a04c6381c96bdf9f4019
Gleb <skryabin.gleb.d@gmail.com>
2023-08-13 23:15:00 +0300

    added ability to load json gruff configuration directly

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,17 +5,27 @@ function draw_info_box() {
     local time=$(date +%H:%M:%S)
     local info="    [$date $time] $*    "
 
-    # tput setaf 3
+    tput setaf 3
 
-    # echo "┏${info//?/━}┓"
-    # echo "┃$(tput setaf 4)$info$(tput setaf 3)┃"
-    # echo "┗${info//?/━}┛"
+    echo "┏${info//?/━}┓"
+    echo "┃$(tput setaf 4)$info$(tput setaf 3)┃"
+    echo "┗${info//?/━}┛"
 
-    # tput sgr 0
+    tput sgr 0
 
-    echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-    echo "┃$info"
-    echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    # echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    # echo "┃$info"
+    # echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+}
+
+readonly AlgoViewDataFolderPath="../public/AlgoViewPage/data"
+readonly AlgoViewDataJSFilePath="../public/AlgoViewPage/data/jsonGraphData.js"
+
+function json_to_js() {
+    # $1 = path_to_file.json
+    draw_info_box "run python3 json2js.py $1 $AlgoViewDataJSFilePath"
+    mkdir -p $AlgoViewDataFolderPath
+    python3 json2js.py $1 $AlgoViewDataJSFilePath
 }
 
 draw_info_box "start run.sh $1"
@@ -25,14 +35,21 @@ cd ./scripts
 rm -rf ../public/AlgoViewPage/data/*
 rm -rf output.json
 
-# input.xml --> output.json
-draw_info_box "run ./main $1"
-./main $1
+if [[ $1 == *.json ]]; then # * is used for pattern matching
+    draw_info_box ".json file has been loaded, just transfer it to the visualization system"
 
-# output.json --> jsonGraphData.js
-echo "\n"
-draw_info_box "run python3 json2js.py output.json ../public/AlgoViewPage/data/jsonGraphData.js"
-mkdir -p ../public/AlgoViewPage/data
-python3 json2js.py output.json ../public/AlgoViewPage/data/jsonGraphData.js
+    # input.json --> jsonGraphData.js
+    json_to_js $1
+else
+    draw_info_box ".js file has been loaded, launch the architect"
+
+    # input.xml --> output.json
+    draw_info_box "run ./main $1"
+    ./main $1
+
+    # output.json --> jsonGraphData.js
+    echo "\n"
+    json_to_js "output.json"
+fi
 
 draw_info_box "done!"
